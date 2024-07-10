77f877fc56335b53ed0fd1871ec2e11785c9805d
Ashifuddin Ahmed <124564585+ahmed-ashif@users.noreply.github.com>
2024-07-10 17:22:28 +0530

    Update 4-Fibonacci_Series.c

--- 4-Fibonacci_Series.c
+++ 4-Fibonacci_Series.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-  int num, a = -1, b = 1, c;
+  int num, a = -1, b = 1, c, i;
 
   printf("Enter a number: ");
   scanf("%d", &num);
 
   printf("Fibonacci series upto %d number is : ", num);
 
-  for (int i = 0; i < num; i++)
+  for (i = 0; i < num; i++)
   {
     c = a + b; //this will return the first element i.e. 0
     printf("%d ", c);
